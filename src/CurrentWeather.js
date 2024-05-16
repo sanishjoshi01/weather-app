eed162b88d9722cf6f9835b99f8649073dddc308
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-05-16 20:19:58 +0545

    current weather styling done

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,6 +1,7 @@
-import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaMapMarkerAlt, FaArrowRight } from "react-icons/fa";
 
 function CurrentWeather({ weatherData }) {
+    console.log(weatherData);
     const country = weatherData.location.country;           //Country Name
     const region = weatherData.location.name;               //Region Name
 
@@ -14,16 +15,38 @@ function CurrentWeather({ weatherData }) {
 
     const feels_like = weatherData.current.feelslike_c;     //Feels Like in degree
 
+    const wind_direction = weatherData.current.wind_dir;
+    const wind_kph = weatherData.current.wind_kph;
+
     return (
         <section>
             <div className="current-weather">
                 <div className="weather-detail">
-                    <div className="location"><FaMapMarkerAlt /> {`${region}, ${country}`}</div>
+                    <div className="location">
+                        <span className="icon">
+                            <FaMapMarkerAlt />
+                        </span>
+                        <div>
+                            {`${region}, ${country}`}
+                        </div>
+                    </div>
                     <p className="current-date">{currentDate}</p>
                 </div>
-                <div className="current-temperature">{currentTemperature}<sup className="celsius">°C</sup></div>
-                <div className="feels-like">
-                    <img src={icon} alt="icon"></img>{`${condition} (Feels like ${feels_like}°C)`}
+                <div className="details-container">
+                    <div className="left-container">
+                        <div className="current-temperature">{currentTemperature}<sup className="celsius">°C</sup></div>
+                        <div className="condition"><img src={icon} alt="icon"></img>{condition}</div>
+                    </div>
+
+                    <div className="right-container">
+                        <div className="feels-like">
+                            {`Feels like ${feels_like}°C`}
+                        </div>
+                        <div className="wind-direction">
+                            <span className="icon"><FaArrowRight /></span>
+                            <div>{wind_direction} wind, {wind_kph} km/h</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
